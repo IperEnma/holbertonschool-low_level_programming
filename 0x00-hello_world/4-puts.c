bb200eca41166b3f21db5dd3eaabfb9f9933a6c9
Enmanuel <4384@holbertonschool.com>
2022-02-03 09:21:40 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
-int main()
+/*Mi primer codigo en C*/
+int main(void)
+
 	{
-		puts (""Programming is like building a multilingual puzzle");
-		return(0)
+		puts("Programming is like building a multilingual puzzle\n");
+		return (0);
 	}
