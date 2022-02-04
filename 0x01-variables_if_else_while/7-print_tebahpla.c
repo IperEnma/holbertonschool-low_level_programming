69205955200c46e0b95d1dd5acb1168393be5cee
Enmanuel <4384@holbertonschool.com>
2022-02-04 11:33:51 -0800

    number-rev

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ *
+ * description: numbers
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '9'; a >= '0'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
