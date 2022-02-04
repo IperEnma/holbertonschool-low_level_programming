d438b8c77896afaed15054026b956d82c729afe8
Enmanuel <4384@holbertonschool.com>
2022-02-04 12:31:38 -0800

    number_coma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ *
+ * description: numbers
+ *
+ */
+int main(void)
+{
+        char a;
+
+        for (a = '0'; a <= '9'; a++)
+        {
+                putchar(a);
+		if (a != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		else
+		{
+		putchar(10);
+		}
+        }
+        putchar('\n');
+        return (0);
+
+}
