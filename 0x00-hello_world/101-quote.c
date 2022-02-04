4293d1d35a734c97412e7c1d62633213db32e5a1
Enmanuel <4384@holbertonschool.com>
2022-02-03 18:22:07 -0800

    quot.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#incluide <stdio.h>
+/**
+ * main - main
+ * Return: 1
+ *
+ * description: stderr, standard error
+ *
+ */
+int main(void)
+	write(STDOUT_FILE, "and that piece of art is usefu\n" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
