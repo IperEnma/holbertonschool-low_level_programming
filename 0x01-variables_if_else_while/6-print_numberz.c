af263b3950517396e1cf10db053834103472d53b
Enmanuel <4384@holbertonschool.com>
2022-02-04 11:21:33 -0800

    numbersint

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  * main - main
+ *   * Return: 0
+ *    *
+ *     * description: numbers
+ *      *
+ *       */
+int main(void)
+{
+	int a;
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+		putchar('\n');
+	return (0);
+}
