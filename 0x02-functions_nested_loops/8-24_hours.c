c46a063676910d27810951e7748d49274fc130be
Enmanuel <4384@holbertonschool.com>
2022-02-07 13:32:54 -0800

    clock

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * jack_bauer - every minute of the day of Jack Bauer
- * @l: character check
+ * nothign void function
  * Return: 1 sucess or 0 in fail
  */
 void jack_bauer(void)
 {
 	int mint, hrs;
 
-for (hrs = 0; hrs < 24; hrs++)
+	for (hrs = 0; hrs < 24; hrs++)
 	{
 		for (mint = 0; mint < 60; mint++)
 		{
