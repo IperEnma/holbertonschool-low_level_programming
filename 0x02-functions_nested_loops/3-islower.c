2ee52392fd0f35c8281f6b2d0b7a6afb9de27ba2
Enmanuel <4384@holbertonschool.com>
2022-02-07 09:00:22 -0800

    return_lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - check if c is lowercase
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c > 60 && c <= 122)
+	{
+	return (0);
+	}
+	else
+	{
+	return (1);
+	}
+}
