2308b7964f1f49c4cad94f2e45c18ac72bf11fa9
Enmanuel <4384@holbertonschool.com>
2022-02-07 04:49:14 -0800

    puthcar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ *
+ * description: printf putchar
+ *
+ */
+int main(void)
+{
+	char caracter = 'a';
+
+	putchar(caracter);
+	putchar('\n');
+	return (0);
+
+}
