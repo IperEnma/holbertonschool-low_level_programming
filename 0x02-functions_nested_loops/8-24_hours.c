14158c37cbdd0b9e458c6e965e0156558c564458
Enmanuel <4384@holbertonschool.com>
2022-02-07 13:28:55 -0800

    clocl

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - every minute of the day of Jack Bauer
+ * @l: character check
+ * Return: 1 sucess or 0 in fail
+ */
+void jack_bauer(void)
+{
+	int mint, hrs;
+
+for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mint = 0; mint < 60; mint++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((mint / 10) + '0');
+			_putchar((mint % 10) + '0');
+			_putchar('\n');
+		}
+
+	}
+}
