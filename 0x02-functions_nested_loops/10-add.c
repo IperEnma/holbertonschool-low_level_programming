dc06eb0f94300a9a6bfe4107ec0e8712bc1da1b5
Enmanuel <4384@holbertonschool.com>
2022-02-07 15:04:37 -0800

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * add - adds two integers and returns the result.
- * nothign void function
+ * @a: value one
+ * @b: value two
  * Return: 1 sucess or 0 in fail
  */
 int add(int a, int b)
