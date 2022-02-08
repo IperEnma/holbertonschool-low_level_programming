ba0eea7c4cc22fa02998aedd42d40e4aecd86c14
Enmanuel <4384@holbertonschool.com>
2022-02-08 11:24:39 -0800

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,7 +10,7 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest ;
+	int largest;
 
 	if (a >= b && a >= c)
 	{
@@ -20,7 +20,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (c >= b && c >= a)
+	else
 	{
 		largest = c;
 	}
