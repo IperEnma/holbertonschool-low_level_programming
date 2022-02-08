fb574248641897707fae32ca9385698ce2d4c0f4
Enmanuel <4384@holbertonschool.com>
2022-02-07 18:47:02 -0800

    3

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#
+/**
+ * print_times_table - prints the 9 times table
+ * @n: table
+ * Return: 1 sucess or 0 in fail
+ */
+int main()
+{	
+	int a;
+	unsigned long int a, sum;
+	unsigned long int n = 0, m = 0;
+
+	for (a = 1; a < 1024; a++)
+		if (a % 3 == 0)
+			m = m + a;
+		else if (a % 5 == 0)
+			n = n + a;
+	sum = n + m;
+	printf("%lu\n", sum);
+	return (0);
+}
+
+
