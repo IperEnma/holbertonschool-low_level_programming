3b0a5958629588758f85573c773df1138f4f80ad
Enmanuel <4384@holbertonschool.com>
2022-02-09 07:31:59 -0800

    mostnumbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,6 +7,7 @@
 void print_numbers(void)
 {
 	int c = 48;
+
 	for (; c >= 48 && c <= 57; c++)
 	_putchar(c);
 	_putchar('\n');
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - Do not print 2 and 4
+ * void function
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int c = 48;
+
+	for (; c >= 48 && c < 57; c++)
+	{
+		if ( c != 50 && c != 52)
+		_putchar(c);
+	}
+	_putchar('\n');
+}
