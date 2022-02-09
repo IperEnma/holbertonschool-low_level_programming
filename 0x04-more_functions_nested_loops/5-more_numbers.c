fc235de1d33b677e8f303e75e3bf17909a640c7c
Enmanuel <4384@holbertonschool.com>
2022-02-09 09:46:36 -0800

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * @
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int c = 0;
+	int c1 = 0;
+	
+	for (; c < 10; c++)
+	{
+		for (; c1 < 15; c1++)
+		{
+			if (c1 > 9)
+			{
+				_putchar((c1 / 10) + 48);
+			}
+			_putchar((c1 % 10) + 48);
+		}
+	}
+}
