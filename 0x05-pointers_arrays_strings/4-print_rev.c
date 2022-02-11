dc99044058eb585cf75ee913a1984f6033a9c51b
Enmanuel <4384@holbertonschool.com>
2022-02-11 11:26:10 -0800

    I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include "string.h"
 /**
- *
- *
- *
+ * print_rev - I've not got a reverse gear
+ * @s: pointer
+ * Return: pointer
  */
 void print_rev(char *s)
 {
 	int i;
 
-	for (i = strlen(s) - 1; i >= 0; i--)
+	for (i = strlen(s); i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
