cb8874527620e12d40fe912a30842b9770cf71d1
Enmanuel <4384@holbertonschool.com>
2022-02-11 15:33:56 -0800

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,19 +13,13 @@ void puts_half(char *str)
 	if (i % 2 == 0)
 	{
 		i = i + 1;
-		i = i / 2;
+		
+	       	= i / 2;
+	}
 
 	for (; str[i]; i++)
 	{
 		_putchar(str[i]);
-	}
-	}
-	else
-	{
-	for (; str[i]; i++)
-	{
-		_putchar(str[i]);
-	}
 	}
 	_putchar('\n');
 }
