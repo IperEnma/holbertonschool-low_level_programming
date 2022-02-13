7ccfe0165a5d64740ba19bdc8df3da7e91e63197
Enmanuel <4384@holbertonschool.com>
2022-02-13 05:53:34 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,14 +12,14 @@ int _atoi(char *s)
 
 	for (; s[i]; i++)
 		if (flag)
-		{	
+		{
+			if (s[i] == '-')
+				si = si * -1;
+			if (s[i] == '+')
+				si = si * 1;
 			if (s[i] >= '0' && s[i] <= '9')
 			{
 				ret = (ret * 10) + (s[i] - 48);
-				if(s[i - 1] == '-')
-				{
-					si = -1;
-				}
 			}
 			if (s[i] <= '0' && s[i] >= '9')
 			{
