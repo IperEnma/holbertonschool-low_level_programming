7048c193aa39a829faf6021f205e0a66ed199e8c
Enmanuel <4384@holbertonschool.com>
2022-02-12 19:04:59 -0800

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+/*^
+ * _atoi - atoi
+ * @flag: sfds
+ * @ret: sdfdsf
+ * @s: lkajsdsad
+ * Return ret
+ */
+int _atoi(char *s)
+{
+	int flag = 1;
+	int ret = 0;
+	int i = 0;
+
+	for (; s[i]; i++)
+		if (flag)
+		{
+			if (s[i] >= '0' && s[i] <= '9')
+			{
+				ret = (ret * 10) + (s[i] - 48);
+					if (s[i] <= '0' && s[i] >= '9')
+					{
+						flag = 0;	
+					}
+			}
+		}
+ 	return (ret);
+}
