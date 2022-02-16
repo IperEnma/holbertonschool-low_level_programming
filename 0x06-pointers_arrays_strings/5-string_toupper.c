005204e47bf25d63207e0f5e936e7663dbaace0c
Enmanuel <4384@holbertonschool.com>
2022-02-16 04:39:46 -0800

    all lowercase letters of a string to uppercase.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- *
- *
- *
+ * string_toupper - all lowercase letters of a string to uppercase
+ * @c: string
+ * Return: c
  */
 char *string_toupper(char *c)
 {
 	int i = 0;
 
-	for(; c[i]; i++)
+	for (; c[i]; i++)
 	{
 		if (c[i] > 96 && c[i] < 123)
 			c[i] = c[i] - 32;
