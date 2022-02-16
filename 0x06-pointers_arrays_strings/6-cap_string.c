048c0b08afa38c17fc1ebde99d4c506e3a89557e
Enmanuel <4384@holbertonschool.com>
2022-02-16 04:53:07 -0800

    capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,24 @@
 /**
- *
- *
- *
+ * cap_string - capitalizes all words of a string.
+ * @a: string
+ * Return: a
  */
 char *cap_string(char *a)
 {
-        int i = 0;
+	int i = 0;
 
-        for (; a[i]; i++)
-        {
-        	 if (a[i] == 9 || a[i] == 10 || a[i] == 32 || a[i] == 44 || a[i] == 59 || a[i] == 46 || a[i] == 40 || a[i] == 41 || a[i] == 34 || a[i] == 63 || a[i] == 33 || a[i] == 123 || a[i] == 125)
-		{		
-                	if (a[i + 1] > 96 && a[i + 1] < 123)
-		 		a[i + 1] = a[i + 1] - 32;
+	if (a[i] > 96 && a[i] < 123)
+		a[i] = a[i] - 32;
+
+	for (; a[i]; i++)
+	{
+		if (a[i] == 9 || a[i] == 10 || a[i] == 32 || a[i] == 44
+				|| a[i] == 59 || a[i] == 46 || a[i] == 40
+				|| a[i] == 41 || a[i] == 34 || a[i] == 63
+				|| a[i] == 33 || a[i] == 123 || a[i] == 125)
+		{
+			if (a[i + 1] > 96 && a[i + 1] < 123)
+				a[i + 1] = a[i + 1] - 32;
 		}
 	}
 	return (a);
