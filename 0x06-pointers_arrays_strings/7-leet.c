2635266113dbba73b2c14aaba2b7e37b17feebbc
Enmanuel <4384@holbertonschool.com>
2022-02-16 04:44:20 -0800

    function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,18 +1,18 @@
 /**
- *
- *
- *
+ * leet - function that encodes a string into 1337
+ * @c: string
+ * Return: c
  */
 char *leet(char *c)
 {
 	int i = 0;
 	int a = 0;
 	int or[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int new[] = {52, 52, 51, 51,48, 48, 55, 55, 49, 49}; 
+	int new[] = {52, 52, 51, 51, 48, 48, 55, 55, 49, 49};
 
 	for (; c[i]; i++)
-	{ 
-		for(a = 0; a < 10; a++)
+	{
+		for (a = 0; a < 10; a++)
 		{
 			if (c[i] == or[a])
 				c[i] = new[a];
