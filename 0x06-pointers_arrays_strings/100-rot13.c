7bf1ea2d47d3d3fbf6937f45ad92e7b9d5c3172b
Enmanuel <4384@holbertonschool.com>
2022-02-16 08:41:05 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,24 @@
 /**
- *
- *
- *
+ * rot13 - encodes a string using rot13
+ * @c: string received
+ * Return: c
  */
 char *rot13(char *c)
 {
 	int i = 0;
+	int a = 0;
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char ori[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 	for (; c[i]; i++)
 	{
-		if ( c[i] > 64 && c[i] < 91)
+		for (a = 0; a < 52; a++)
 		{
-			if (c[i] > 77 && c[i] < 91)
-				c[i] = c[i] + 13;
-			if (c[i] > 64 && c[i] < 78)
-				c[i] = c[i] - 13;
-		}
-
-		if ( c[i] > 96 && c[i] < 123)
-		{
-			if (c[i] > 109 && c[i] < 123)
-				c[i] = c[i] + 13;
-			if (c[i] > 96 && c[i] < 110)
-				c[i] = c[i] - 13;
+			if (c[i] == ori[a])
+			{
+				c[i] = rot13[a];
+				break;
+			}
 		}
 
 	}
