c0838cada92a1453ff22e2ef91643ecb593d2079
Enmanuel <4384@holbertonschool.com>
2022-02-21 18:41:28 -0800

    add

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int j = size - 1;
+	int suma1 = 0;
+	int suma2 = 0;
+
+	for (; i <= (size * size);)
+	{
+		suma1 = suma1 + a[i];
+		i = i + size + 1;
+	}
+
+	for (; j < (size * size - 1);)
+	{
+		suma2= suma2 + a[j];
+		j = j + size - 1;	
+	}
+
+	printf("%d, %d\n", suma1, suma2);
+}
