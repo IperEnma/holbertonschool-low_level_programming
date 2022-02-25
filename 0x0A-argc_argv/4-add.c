3ae8dd895af057d9088237f0265a8b4c221c367a
IperEnma <enmanuelhernandez1843.com>
2022-02-25 09:37:45 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
+ * main - adds positive numbers.
+ * @argc: size of the argv array
+ * @argv: An array of size argc
+ * Return: 1 or 0
  */
 int main(int argc, char *argv[])
 {
 	int suma = 0;
 	int i = 1;
+
+	if (argc < 1)
+		return (0);
 
 	for (i = 1; i < argc; i++)
 	{
@@ -21,5 +25,5 @@ int main(int argc, char *argv[])
 	}
 	printf("%d\n", suma);
 
-	return(0);
+	return (0);
 }
