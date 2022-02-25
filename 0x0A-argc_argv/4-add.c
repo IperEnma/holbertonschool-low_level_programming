3e72d12625b93589d5076ecfac619a8ac77e38f7
IperEnma <enmanuelhernandez1843.com>
2022-02-25 10:13:26 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j]; j ++)
-		{	
+		for (j = 0; argv[i][j]; j++)
+		{
 			if ((argv[i][j]) < 48 || (argv[i][j]) > 57)
 			{
 				printf("Error\n");
