cfd7dbe45de83d05f0fded1557180b7ec1b7ad81
IperEnma <enmanuelhernandez1843.com>
2022-03-01 12:01:07 -0800

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 /**
  * wildcmp - function that compares two string
  * @s1: string 1
@@ -6,31 +8,36 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '*')
-		return (wildcmp(s1, s2 + 1));
-	if (*s1  != '\0' && *s2 == '\0')
-		return (0);
-	if (*s1  == '\0' && *s2 != '\0')
-		return (0);
-	if (*s1  == '\0' && *s2 == '\0')
+
+	/* reach the end of both strings */
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
+	/*
+	 * if found wildcard in s2 continue with next character
+	 * in s2 with recursion
+	 */
+	if (*s1 == '\0' && *s2 != '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, (s2 + 1)));
+	/* both characaters are the same, then increase both in +1 */
 	if (*s1 == *s2)
-	{
-		if (*(s2 - 1) != '*')
-		{
-			if (*(s2 - 1) != *(s1 - 1))
-				return (0);
-		}
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
+		return (wildcmp((s1 + 1), (s2 + 1)));
+	/* if wildcard */
 	if (*s2 == '*')
 	{
-		if (*(s2 + 1) == '\0')
+		/* next character is too a wildcard */
+		if (*(s2 + 1) == '*')
+			/* recursion with next s2 */
+			return (wildcmp(s1, (s2 + 1)));
+		/* wildcard match */
+		if (wildcmp(s1, s2 + 1) != '\0')
 			return (1);
-		return (wildcmp(s1, s2 + 1));
-	}
-	if (*s2 != '*')
+		/**
+		 * must continue comparing nex character in s1
+		 * using same wildcard in s2
+		 */
 		return (wildcmp(s1 + 1, s2));
+	}
 	return (0);
-
 }
