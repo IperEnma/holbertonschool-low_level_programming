9bcd63c3a2ab771b9dd716b913f6981569dd52b6
IperEnma <enmanuelhernandez1843.com>
2022-03-02 05:20:45 -0800

    malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,12 @@ char *create_array(unsigned int size, char c)
 	if (size > 0)
 	{
 		p = malloc(size);
+		if (p != NULL)
+		{
 		for (i = 0; i <= size; i++)
 		{
 			p[i] = c;
+		}
 		}
 	}
 	return (p);
