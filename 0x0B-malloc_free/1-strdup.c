e203d683ea54cefe95c82ec8228df6069a00be61
IperEnma <enmanuelhernandez1843.com>
2022-03-02 05:37:25 -0800

    contains a copy of the string given as a parameter.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * _strdup - contains a copy of the string given as a parameter.
+ * @str: string
+ * Return: p
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *p = NULL;
+
+	if (str != NULL)
+	{
+		p = malloc(sizeof(str));
+		if (p != NULL)
+		{
+		for (i = 0; str[i]; i++)
+		{
+			p[i] = str[i];
+		}
+		}
+
+	}
+	return (p);
+}
