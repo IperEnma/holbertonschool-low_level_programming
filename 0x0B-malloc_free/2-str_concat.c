4e7a2425674e925c49d1ea58fdf9f32942fa654f
IperEnma <enmanuelhernandez1843.com>
2022-03-02 06:18:43 -0800

    concatenar

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ * str_concat - concatenates two strings.
+ * @s1: string 1
+ * @s2: string 2
+ * Return
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p = NULL;
+	int i, j = 0;
+	
+	if (s1 != NULL && s2 != NULL)
+	{
+		p = malloc((strlen(s1) + 1) + (strlen(s2) + 1));
+			if (p != NULL)
+			{
+				for (i = 0; s1[i]; i++)
+				{
+					p[i] = s1[i];
+				}
+				for (j = 0; s2[j]; j++)
+				{
+					p[i] = s2[j];
+					i++;
+				}
+			}
+	}
+
+	return (p);
+}
