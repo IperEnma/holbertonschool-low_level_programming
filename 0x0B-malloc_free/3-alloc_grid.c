70fe23a76698ea79deb60804e0e399d520fa8952
IperEnma <enmanuelhernandez1843.com>
2022-03-02 10:38:30 -0800

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,8 +18,8 @@ int **alloc_grid(int width, int height)
 	p = (int **)malloc(height * sizeof(int *));
 	if (p != NULL)
 	{
-	for (i = 0; i < height; i++)
-		p[i] =(int *)malloc(width * sizeof(int));
+		for (i = 0; i < height; i++)
+			p[i] =(int *)malloc(width * sizeof(int));
 
 		for (i = 0; p[i]; i++)
 		{
