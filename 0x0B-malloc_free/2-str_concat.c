27e48ddf6daf2ffc2fe0d8e348b80178a068809f
IperEnma <enmanuelhernandez1843.com>
2022-03-02 06:30:02 -0800

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,19 +14,24 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 != NULL && s2 != NULL)
 	{
-		p = malloc(strlen(s1) + strlen(s2) + 1);
-			if (p != NULL)
+		if (s1 == NULL)
+			p = malloc(strlen(s2) + 1);
+		else if (s2 == NULL)
+			p = malloc(strlen(s1) + 1);
+		else
+			p = malloc(strlen(s1) + strlen(s2) + 1);
+		if (p != NULL)
+		{
+			for (i = 0; s1[i]; i++)
 			{
-				for (i = 0; s1[i]; i++)
-				{
-					p[i] = s1[i];
-				}
-				for (j = 0; s2[j]; j++)
-				{
-					p[i] = s2[j];
-					i++;
-				}
+				p[i] = s1[i];
 			}
+			for (j = 0; s2[j]; j++)
+			{
+				p[i] = s2[j];
+				i++;
+			}
+		}
 	}
 
 	return (p);
