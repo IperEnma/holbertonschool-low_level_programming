79820fc7b706cd01fc083c45c8f36c15982f4220
IperEnma <enmanuelhernandez1843.com>
2022-03-03 07:57:56 -0800

    malloc, free, calloc, more

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,15 +16,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	p = malloc(strlen(s1) + strlen(s2) - 1);
+	p = malloc(strlen(s1) + strlen(s2));
 
 	if (p != NULL)
 	{
 		for (i = 0; s1[i]; i++)
 			p[i] = s1[i];
 		for (j = 0; s2[j] && j < n; j++)
-		p[i++] = s2[j];
+			p[i++] = s2[j];
+		p[i] = '\0';
 	}
-	p[i] = '\0';
 	return (p);
 }
