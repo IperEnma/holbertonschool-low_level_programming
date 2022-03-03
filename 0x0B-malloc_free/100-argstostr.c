e137583878a8ed6a0a6f38802ca8cb8841ab155d
IperEnma <enmanuelhernandez1843.com>
2022-03-02 17:18:36 -0800

    concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,15 +3,16 @@
 #include <string.h>
 #include <stdio.h>
 /**
- *
- *
- *
+ * argstostr - concatenates all the arguments of your program
+ * @ac: size array
+ * @av: array
+ * Return: p
  */
 char *argstostr(int ac, char **av)
 {
 	char *p = NULL;
 	int i, j, t, pi = 0;
-	
+
 
 	if (ac == 0 || av == NULL)
 		return (p);
@@ -30,7 +31,7 @@ char *argstostr(int ac, char **av)
 	{
 		for (i = 0; i < ac; i++)
 		{
-			for(j = 0; av[i][j]; j++)
+			for (j = 0; av[i][j]; j++)
 			{
 				p[pi] = av[i][j];
 				pi++;
