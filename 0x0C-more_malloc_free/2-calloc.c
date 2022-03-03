a38882de7af77fce4d5da3088048bd13fa65492b
IperEnma <enmanuelhernandez1843.com>
2022-03-03 09:26:03 -0800

    malloc, calloc, free, more

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i = 0;
 	char *p = NULL;
 
-	if (((nmemb != 0) && (size != 0))
+	if ((nmemb != 0) && (size != 0))
 	{
 		p = malloc(nmemb * size);
 		if (p != NULL)
