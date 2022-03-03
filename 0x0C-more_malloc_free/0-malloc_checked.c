ce6638afbf5fb08780ee1e7c48a218baae6ae157
IperEnma <enmanuelhernandez1843.com>
2022-03-03 07:06:19 -0800

    more malloc,calloc, free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,14 +2,14 @@
 /**
  * malloc_checked - function that allocates memory using malloc
  * @b: allocated memory
- * Return:
+ * Return: pointer
  */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
-
+	
 	p = malloc(b);
 	if (p == NULL)
 		exit (98);
-	return(p);
+	return (p);
 }
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - returned pointer shall point to a newly allocated space in memory
+ * @s1: string 1
+ * @s2: string 2
+ * @n:
+ * Return:
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int s11 = 0, s12 = 0, j = 0;
+	int i = 0;
+	char *p = NULL;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	s11 = strlen(s1);
+	s12 = strlen(s2);
+	if ( n >= s12)
+		s12 = n;
+	p = malloc(s11 + s12 + 1);
+	if (p != NULL)
+	{
+		for (i = 0; s1[i]; i++)
+			p[i] = s1[i];
+		for (j = 0; j < n; j++)
+		{
+			p[i] = s2[j];
+			i++;
+		}
+	}
+	return (p);
+}
