ec11ee2664bf657d5016a62432b4ba2115095ba2
IperEnma <enmanuelhernandez1843.com>
2022-03-02 17:15:31 -0800

    argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+char *argstostr(int ac, char **av)
+{
+	char *p = NULL;
+	int i, j, t, pi = 0;
+	
+
+	if (ac == 0 || av == NULL)
+		return (p);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[j]; j++)
+		{
+			t++;
+		}
+		t++;
+	}
+
+	p = malloc(t + 1);
+
+	if (p != NULL)
+	{
+		for (i = 0; i < ac; i++)
+		{
+			for(j = 0; av[i][j]; j++)
+			{
+				p[pi] = av[i][j];
+				pi++;
+			}
+			p[pi] = '\n';
+			pi++;
+		}
+	}
+		return (p);
+}
