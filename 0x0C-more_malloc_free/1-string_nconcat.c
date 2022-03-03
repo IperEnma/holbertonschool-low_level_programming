bdb7a609b1c4bd22a0ad5bae55b96e7bcbccebfc
IperEnma <enmanuelhernandez1843.com>
2022-03-03 07:52:42 -0800

    malloc, calloc, free, more

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,35 +9,21 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s11 = 0, s12 = 0, j = 0;
-	int i = 0;
+	unsigned int j = 0, i = 0;
 	char *p = NULL;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	s11 = strlen(s1);
-	s12 = strlen(s2);
-	if (n >= s12)
-		p = malloc(s11 + s12 + 1);
-	if (n < s12)
-		p = malloc(s11 + n + 1);
+	p = malloc(strlen(s1) + strlen(s2) + 1);
+
 	if (p != NULL)
 	{
 		for (i = 0; s1[i]; i++)
 			p[i] = s1[i];
-		if (n >= s12)
-		{
-			for (j = 0; j < s12; j++)
-				p[i++] = s2[j];
-
-		}
-		if (n < s12)
-		{
-			for (j = 0; j < n; j++)
-			p[i++] = s2[j];
-		}
+		for (j = 0; s2[j] && j < n; j++)
+		p[i++] = s2[j];
 	}
 	return (p);
 }
