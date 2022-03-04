2afe556aebcd42b575daccc431c86cd7895f6373
IperEnma <enmanuelhernandez1843.com>
2022-03-03 16:35:45 -0800

    reallocate

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: size of ptr
+ * @new_size: new size
+ * Return: ptr
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *s;
+
+	if (new_size == 0 && ptr != NULL)
+		return(ptr);
+	if (ptr == NULL)
+	{
+		s = malloc(new_size);
+	}
+	if (new_size > old_size)
+	{
+		s = malloc(new_size);
+		return (s);
+	}
+	if (old_size == new_size)
+		return (ptr);
+	return (ptr);
+}
