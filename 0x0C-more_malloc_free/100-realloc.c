fa79c5020edbf7a2c387a8c830cd7bcb9543b1ee
IperEnma <enmanuelhernandez1843.com>
2022-03-03 17:02:11 -0800

    reallocate

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,7 +11,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *s;
 
 	if (new_size == 0 && ptr != NULL)
+	{
 		free(ptr);
+		return (0);
+	}
 	if (ptr == NULL)
 	{
 		s = malloc(new_size);
@@ -26,5 +29,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (old_size == new_size)
 		return (ptr);
-	return (ptr);
+	return (0);
 }
