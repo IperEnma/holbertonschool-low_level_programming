d8e751da49189d2467917261e2c655aab202b6fa
IperEnma <enmanuelhernandez1843.com>
2022-03-08 12:17:33 -0800

    dog new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,19 +15,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *b;
 
 	b = malloc(sizeof(dog_t));
+	if(b)
+	{
 
-	(*b).name = malloc(strlen(name) + 1);
-	if ((*b).name)
-	{
-		for (i = 0; name[i]; i++)
-			(*b).name[i] = name[i];
-	}
-	(*b).age = age;
-	(*b).owner = malloc(strlen(owner) + 1);
-	if ((*b).owner)
-	{
-		for (i = 0; owner[i]; i++)
-			(*b).owner[i] = owner[i];
+		(*b).name = malloc(strlen(name) + 1);
+		if ((*b).name)
+		{
+			for (i = 0; name[i]; i++)
+				(*b).name[i] = name[i];
+		}
+		(*b).age = age;
+		(*b).owner = malloc(strlen(owner) + 1);
+		if ((*b).owner)
+		{
+			for (i = 0; owner[i]; i++)
+				(*b).owner[i] = owner[i];
+		}
 	}
 	return (b);
 
