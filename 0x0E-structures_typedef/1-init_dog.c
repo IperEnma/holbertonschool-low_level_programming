ec531583fc0d5bbd4f9d7b8c2522c783f1b460d6
IperEnma <enmanuelhernandez1843.com>
2022-03-08 09:29:51 -0800

    struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,11 @@
 #include "dog.h"
 /**
- *
- *
- *
+ * init_dog - initialize data
+ * @d: i a pointer
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner dog|:w
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
