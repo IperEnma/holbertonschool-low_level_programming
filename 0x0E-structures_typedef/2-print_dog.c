b39ba886970f46f966a796f1b2e8014e1cc5cfad
IperEnma <enmanuelhernandez1843.com>
2022-03-08 10:41:47 -0800

    initialize data

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,13 +13,13 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", (*d).name);
 		else
 			printf("Name: (nil)\n");
-	/*	if ((*d).age > 0)
+		if ((*d).age > 0)
 			printf("Age: %f\n", (*d).age);
 		else
-			printf("(nil)\n");*/
+			printf("(nil)\n");
 		if ((*d).owner != NULL)
 			printf("Owner: %s\n", (*d).owner);
 		else
-			printf("(nil)\n");
+			printf("Owner: (nil)\n");
 	}
 }
