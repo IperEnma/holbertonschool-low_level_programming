7cf991735add044f9c47c9e1be571e5eee91d8ac
IperEnma <enmanuelhernandez1843.com>
2022-03-09 15:22:27 -0800

    fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,15 +11,15 @@ int main(int argc, char *argv[])
 {
 	int one = 0;
 	int two = 0;
-	int (*function)(int, int);
+	op_t function;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2][0] != '+' && argv[2][0] != '-' &&
-	argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%') || argv[2][1] != '\0')
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0]
+	!= '*' && argv[2][0] != '/' && argv[2][0] != '%') || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
@@ -28,9 +28,9 @@ int main(int argc, char *argv[])
 	one = atoi(argv[1]);
 	two = atoi(argv[3]);
 
-	function = get_op_func(argv[2]);
+	function.f = get_op_func(argv[2]);
 
-	printf("%d\n", function(one, two));
+	printf("%d\n", function.f(one, two));
 
 	return (0);
 }
