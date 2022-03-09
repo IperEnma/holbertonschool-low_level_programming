604d850bba9b0a922c13d1834c51c22b0eb2e6a4
IperEnma <enmanuelhernandez1843.com>
2022-03-09 15:19:24 -0800

    fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,12 +18,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2][0] != '+' && argv[2][0] != '-' &&
-	argv[2][0] != '*' && argv[2][0] != '/') || argv[2][0] != '%')
+	if (argv[2][0] != '+' && argv[2][0] != '-' &&
+	argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%' || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	one = atoi(argv[1]);
 	two = atoi(argv[3]);
 
