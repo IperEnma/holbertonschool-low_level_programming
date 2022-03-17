8c5fb63e2e1e3ce76c406df3ff1f0f09869aafdc
IperEnma <enmanuelhernandez1843.com>
2022-03-17 15:20:40 -0700

    add node end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: head node
+ * @str: string
+ * Return: new
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *s;
+	list_t *headaux = *head;
+	list_t *new;
+
+	s = strdup(str);
+	new = malloc(sizeof(list_t));
+
+	new->str = s;
+	new->len = strlen(s);
+	new->next = NULL;
+
+	if (s && *head)
+	{
+		for (; headaux->next;)
+		{
+			headaux = headaux->next;
+		}
+		new->next = NULL;
+		headaux->next = new;
+	}
+	else
+	{
+		*head = new;
+	}
+	return (new);
+}
