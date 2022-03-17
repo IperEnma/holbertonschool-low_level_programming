9949fe5fa36d6623021e5eff77a3b0b008ffd973
IperEnma <enmanuelhernandez1843.com>
2022-03-17 11:50:03 -0700

    addnode

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,12 +12,14 @@ list_t *add_node(list_t **head, const char *str)
 
 	s = strdup(str);
 
+	if (s && head)
+	{
 		NodoNew = malloc(sizeof(list_t));
 
 		NodoNew->str = s;
 		NodoNew->len = strlen(s);
 		NodoNew->next = *head;
 		*head = NodoNew;
-
+	}
 	return (NodoNew);
 }
