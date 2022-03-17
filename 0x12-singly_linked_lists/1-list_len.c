4c9eb48d5f73eba140c7c0f7eed16713859fde9c
IperEnma <enmanuelhernandez1843.com>
2022-03-17 09:46:34 -0700

    number of elements in a linked list_t

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include "lists.h"
+/**
+ * list_len - number of elements in a linked list_t
+ * @h: head
+ * Return: ret
+ */
+size_t list_len(const list_t *h)
+{
+	size_t ret = 0;
+
+	for (; h;)
+	{
+		if (h->str)
+			ret++;
+		h = h->next;
+	}	
+	return (ret);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,6 +11,6 @@ typedef struct Node
 	int len;
 	struct Node *next;
 } list_t;
-
+size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 #endif
