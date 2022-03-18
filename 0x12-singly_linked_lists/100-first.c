efe4884305e41082a4767b1013bd4c15153832b8
IperEnma <enmanuelhernandez1843.com>
2022-03-18 12:49:48 -0700

    ++

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * before_main - before the main function is executed.
+ * Return: void
+ */
+void __attribute__ ((constructor)) before_main()
+{	
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
