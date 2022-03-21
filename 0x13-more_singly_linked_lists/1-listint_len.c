7ca782e6d799b1056ea3739ea0364871efd64a11
IperEnma <enmanuelhernandez1843.com>
2022-03-21 07:22:31 -0700

    number elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - number of elements in a linked listint_t list
+ * @h: header
+ * Return: n
+ */
+size_t listint_len(const listint_t *h)
+{
+
+	size_t n = 0;
+
+	for(n = 0; h; n++)
+	{
+		h = h->next;
+	}
+	return (n);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,4 +8,5 @@ typedef struct nodos
 	struct nodos * next;
 } listint_t;
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 #endif
