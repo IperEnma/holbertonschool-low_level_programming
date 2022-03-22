ea4e13fa4dc458e0c5cec7551f031c440fa604af
IperEnma <enmanuelhernandez1843.com>
2022-03-22 12:27:29 -0700

    fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	for (i = 0; i < (idx - 1); i++)
 	{
-		if (!auxp || !(auxp->next))
+		if (!auxp)
 			return (NULL);
 		auxp = auxp->next;
 	}
