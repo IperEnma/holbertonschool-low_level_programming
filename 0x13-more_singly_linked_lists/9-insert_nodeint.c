dfd0a23fc5f5d455d1954e35fe28f2ed0cffe9e4
IperEnma <enmanuelhernandez1843.com>
2022-03-22 10:56:31 -0700

    fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *auxp = *head, *new;
 
-	if (!(*head))
+	if (head)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (!new)
