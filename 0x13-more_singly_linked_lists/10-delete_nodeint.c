cf5f964abca00b09aceeb53fc0801d63804b2be2
IperEnma <enmanuelhernandez1843.com>
2022-03-22 12:11:51 -0700

    fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	for (i = 0; i < (index - 1); i++)
 	{
-		if (!aux)
+		if (!(aux->next)
 			return (-1);
 		aux = aux->next;
 	}
