e07c5f742ed13af9141fcf2c9819f07598b6367a
IperEnma <enmanuelhernandez1843.com>
2022-03-22 06:20:52 -0700

    solved

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,18 +6,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *aux = *head;
 	listint_t *freeaux;
 
 	if (head == NULL || *head == NULL)
 		return;
 
-	for (; aux;)
+	for (; *head;)
 	{
-		freeaux = aux;
-		aux = aux->next;
-		free(freeaux);
+		freeaux = (*head)->next;
+		free(*head);
+		*head = freeaux;
 	}
 	*head = NULL;
-	head = NULL;
 }
