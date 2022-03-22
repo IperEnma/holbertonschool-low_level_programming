aa659692f0098434b26232b12359c05fcc20b8c8
IperEnma <enmanuelhernandez1843.com>
2022-03-22 08:36:25 -0700

    solved

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (auxp)
 	{
-		if (i < idx)
+		if (i < idx - 1)
 		{
 			auxp = auxp->next;
 			i++;
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		else
 			break;	
 	}
-	if (i == idx)
+	if (i + 1 == idx)
 	{
 		new->next = auxp->next;
 		auxp->next = new;
