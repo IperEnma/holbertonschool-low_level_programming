862974e8f6d1cf221eb791c07281bd7bdab44e87
IperEnma <enmanuelhernandez1843.com>
2022-03-22 17:35:38 -0700

    loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -14,7 +14,7 @@ listint_t *find_listint_loop(listint_t *head)
 	slow = slow->next;
 	fast = fast->next->next;
 
-	for (; fast && faste->next;)
+	for (; fast && fast->next;)
 	{
 		if (fast == slow)
 		{
