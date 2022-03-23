9a6df88da92d33e6e2afff5e8e11ecb2c30b8e9b
IperEnma <enmanuelhernandez1843.com>
2022-03-23 09:57:57 -0700

    solved

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,17 +1,38 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * print_listint_safe - that prints a listint_t linked list
+ * @head: head node
+ * Return: i
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i;
+	size_t i = 0, flag = 0;
+	listint_t *end;
+	const listint_t *aux;
+	aux = head;
 
-	for (i = 0; head; i++)
+	end = find_listint_loop((void*)head);
+
+	if (end)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
+		while(flag < 3)
+		{
+			if (aux == end)
+				flag++;
+			if (flag == 2)
+				break;
+			printf("[%p] %d\n", (void *)aux, aux->n);
+			aux = aux->next;
+			i++;
+		}
+	}
+	else
+	{
+		while (aux)
+		{
+			printf("[%p] %d\n", (void *)aux, aux->n);
+			aux = aux->next;
+		}
 	}
 
 	return (i);
