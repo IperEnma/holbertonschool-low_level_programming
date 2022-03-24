28593037f3687770094c656d6ab6e38a40b302a8
IperEnma <enmanuelhernandez1843.com>
2022-03-24 12:30:12 -0700

    print index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_bit - value of a bit at a given index
+ * @index: index
+ * Return: 
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	n = n >> 2;
+	if ((n & 1) == 0)
+		return (0);
+	else
+		return (1);
+
+}
