df858abf29bca47349749779b0b1a2a90cfb3ac5
IperEnma <enmanuelhernandez1843.com>
2022-03-27 09:52:03 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *
+ * DESCRIFRANDO CLAVE DEL CRACKME
+ */
+int main()
+
+{
+	char pass[100];
+	char aux[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	int a = 0, i = 0, count = 0, s = 0;
+	srand(time(NULL));
+	while ((2772 - 122) > count)
+	{
+		s = rand() % 62;
+		pass[i] = s;
+		count += pass[i];
+		i++;
+	}
+	while (aux[a])
+	{
+		if (aux[a] == (2772 - count))
+		{
+			pass[i] = aux[a];
+			count += pass[i];
+			i++;
+			break;
+		}
+		a++;
+	}
+	pass[i] = '\0';
+	printf ("%s", pass);
+	return (0);
+}
