bde5d540113d0640ce511834f89cfb31ce803b7b
IperEnma <enmanuelhernandez1843.com>
2022-03-28 14:57:05 -0700

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 		exit(98);
 	}
-	sd = open(argv[2], O_CREAT | O_WRONLY, 0664);
+	sd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (sd == -1)
 	{
 		closing(sf);
