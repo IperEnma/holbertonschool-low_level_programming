34a51e9faccde18d559aac79c58c1903438f0b8d
IperEnma <enmanuelhernandez1843.com>
2022-03-28 10:36:11 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR, S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd < 0)
 	{
@@ -23,11 +23,13 @@ int create_file(const char *filename, char *text_content)
 	n = strlen(text_content);
 	check = write(fd, text_content, n);
 
-	/* if (check == -1)
+	if (check == -1)
 	{
-		write(STDOUT_FILENO, "fail\n", 5);
+		close(fd);
 		return (-1);
-	}*/
+	}
+	
+	close(fd);
 
 	return (1);
 }
