3c272ef8994ba7bc4c6c67af2d397ac2e40bc022
IperEnma <enmanuelhernandez1843.com>
2022-03-28 11:37:26 -0700

    appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,17 +1,29 @@
 /**
- *
- *
- *
+ * append_text_to_file - appends text at the end of a file
+ * @text_content - text content
+ * @filename: file name
+ * Return: 1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd = 0;
+	int fd = 0, n = 0;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_RDWR | O_APPEND, 0600);
 
 	if (fd == -1)
+	{
+		close(fd);
 		return (-1);
-	wrie
+	}
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	n = strlen(text_content);
+	write(fd, text_content, n);
+	close(fd);
+	return(1);
 }
