36050d542a59711a6ca9e9e236375b3a345ff8ed
IperEnma <enmanuelhernandez1843.com>
2022-03-28 15:04:57 -0700

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -32,11 +32,11 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (!argv[1])
+	/**if (!argv[1])
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-	}
+	}*/
 	sf = open(argv[1], O_RDONLY);
 	if (sf == -1)
 	{
