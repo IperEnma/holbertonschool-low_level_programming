73409e3141033d8efa1e9600b16beff41a5f8b6f
IperEnma <enmanuelhernandez1843.com>
2022-03-28 09:12:59 -0700

    read file

---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+input/Output operations such as open, close, read, write and append, all of which deal with standard disk or tape files
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef IO
+#define IO
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
