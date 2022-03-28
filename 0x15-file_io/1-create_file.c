48f1a6f46db95758d6aa2665e0d445270ca1b1e1
IperEnma <enmanuelhernandez1843.com>
2022-03-28 09:47:30 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: file name
+ * @text_content: content tex
+ * Return: -1 or 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = 0, n = 0, check = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR, S_IWUSR);
+
+	if (fd < 0)
+		return (-1);
+	
+	n = strlen(text_content);
+
+	check = write (fd, text_content, n);
+
+	if (check == -1)
+	{
+		write (STDOUT_FILENO, "fail", 4);
+		return (-1);
+	}
+
+	return (1);
+}
