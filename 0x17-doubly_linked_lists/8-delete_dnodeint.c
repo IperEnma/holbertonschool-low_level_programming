7f73863e8d1b91442532274ed0eb43787e95a096
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-05 12:00:41 -0700

    delete

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,6 +11,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
 	dlistint_t *aux;
+
 	if (*head)
 	{
 		aux = *head;
@@ -28,8 +29,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			aux->prev = NULL;
 			free(aux);
 			return (1);
-		}
-		
+		}	
 	}
 	return (-1);
 }
