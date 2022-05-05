a1fed26440060ddcb2da27a0ce3ed75e9ee1baa4
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-05 09:35:04 -0700

    insert by index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	for (i = 0; i < idx && aux->next; i++)
 		aux = aux->next;
-
+	if (i == idx && aux->next == NULL)
+		return (add_dnodeint_end(h, n));
 	if (i == idx)
 	{
 		aux = aux->prev;
