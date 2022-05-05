806f3cf81ff4e33d8c9a0731c0d9c6a9bcb4a375
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-05 12:45:49 -0700

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ *
+ *
+ */
+unsigned int lenlist(dlistint_t **h)
+{
+	unsigned int i = 0;
+	dlistint_t *aux = *h;
+
+	for (i  = 0; aux; i++)
+	{
+		aux = aux->next;
+	}
+	return (i);
+}
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: header of list
  * @idx: index insert node
@@ -11,34 +26,28 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *aux;
-	unsigned int i = 0;
-
+	unsigned int len = 0;
+	
+	if (idx != 0 && *h == NULL)
+		return (NULL);
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
-	aux = *h;
-	for (i = 0; i < idx && *h && aux->next; i++)
-		aux = aux->next;
-	if (idx == i + 1 && *h)
-		add_dnodeint_end(h, n);
+	len = lenlist(h);
+	if (idx == len)
+		return (add_dnodeint_end(h, n));
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
-		dprintf(2, "Error: Can't malloc\n");
 		return (NULL);
-	}
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
-	if (i == idx && *h)
-	{
-		aux->prev->next = new;
-		new->next = aux;
-		new->prev = aux->prev;
-		aux->prev = new;
-		return (new);
-	}
-	free(new);
-	return (NULL);
+	aux = *h;
+	for (; idx != 0; idx--)
+		aux = aux->next;
+	aux->prev->next = new;
+	new->prev = aux->prev;
+	new->next = aux;
+	aux->prev = new;
+	return (new);
 }
--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -13,18 +13,21 @@ int main(void)
     dlistint_t *head;
 
     head = NULL;
-    /*add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 0);
     add_dnodeint_end(&head, 1);
     add_dnodeint_end(&head, 2);
     add_dnodeint_end(&head, 3);
     add_dnodeint_end(&head, 4);
     add_dnodeint_end(&head, 98);
     add_dnodeint_end(&head, 402);
-    add_dnodeint_end(&head, 1024);*/
+    add_dnodeint_end(&head, 1024);
     print_dlistint(head);
     printf("-----------------\n");
     printf("-----------------\n");
-    insert_dnodeint_at_index(&head, 2, 4096);
+    insert_dnodeint_at_index(&head, 0, 4096);
+    insert_dnodeint_at_index(&head, 4, 4096);
+    insert_dnodeint_at_index(&head, 8, 4096);
+    insert_dnodeint_at_index(&head, 12, 4096);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
