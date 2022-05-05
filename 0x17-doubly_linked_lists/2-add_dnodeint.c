bc1abab47f112953aae6c7da21fbf0f733d16102
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-05 08:38:09 -0700

    double linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,10 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
-		dprintf(2, "Error: Can't malloc\n");
 		return (NULL);
-	}
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
