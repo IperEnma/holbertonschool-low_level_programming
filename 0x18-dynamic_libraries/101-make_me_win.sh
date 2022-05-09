0748679ad93540fdfb0484711f0e43b044a4dee2
IperEnma <98028486+IperEnma@users.noreply.github.com>
2022-05-09 19:22:02 -0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,1 +1,3 @@
-
+echo "static int idx = 0; int rand() { int list[6] = {8, 8, 7, 9 ,23 , 74};idx++; return (list[idx]); }" > /root/injector.c
+gcc /root/injector.c -c -fPIC && gcc injector.o -shared -o /root/library.so
+export LD_PRELOAD=/root/library.so
