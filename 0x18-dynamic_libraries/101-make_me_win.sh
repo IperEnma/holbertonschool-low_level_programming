f45ac369a4ba05a7d73def147d925b3e18cc01f1
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-09 17:40:11 -0700

    injected

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/IperEnma/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/injector.so
+wget -O /tmp/injector.so https://github.com/IperEnma/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/injector.so?raw=true
 export LD_PRELOAD=/tmp/injector.so
