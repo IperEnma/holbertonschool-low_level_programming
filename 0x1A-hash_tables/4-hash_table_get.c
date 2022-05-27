25355002bab5228c635a87a5a84ceeaf753080f9
IperENma <enmanuelhernandez1843@gmail.com>
2022-05-27 06:01:32 -0700

    betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,6 +10,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = 0;
 	const hash_node_t *aux;
 
+	if (ht == NULL)
+		return (NULL);
+
 	index = key_index((unsigned char *)key, ht->size);
 
 	if (ht->array[index] == NULL)
